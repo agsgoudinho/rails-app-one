75f0e8dda6eb1a4767c8e3c699094479f63cebcb
agsgoudinho <aguinaldo@alunos.utfpr.edu.br>
2019-09-10 19:18:41 -0300

    Added the contacts model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates_presence_of :name
   validates_uniqueness_of :name
   validates_numericality_of :age, greater_than: 0, allow_nil: true
+  has_many :contacts
 end
