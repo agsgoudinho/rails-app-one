0d2a83e3e3ab1b7cd5cedec4353a9ebeed82b0d3
agsgoudinho <aguinaldo@alunos.utfpr.edu.br>
2019-09-10 18:34:57 -0300

    Added validations on users model

---
+++ test/models/user_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+
+  test "create a simple user" do
+    assert_difference 'User.all.count', 1 do
+      User.create name: 'user 01'
+    end
+  end
+
+  test "don't create a user without name" do
+    assert_difference 'User.all.count', 0 do
+      User.create bio: 'anything'
+    end
+  end
+
+  test "don’t create duplicated user" do
+    assert_difference 'User.all.count', 1 do
+      User.create name: 'user 01'
+      User.create name: 'user 01'
+    end
+  end
+
+  test "don`t create user unless born" do
+    assert_difference 'User.all.count', 0 do
+      User.create name: 'user 01', age: -1
+    end
+  end
+end
